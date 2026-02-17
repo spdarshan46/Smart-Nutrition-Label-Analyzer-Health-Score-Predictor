651541d90b88407450bdcaaef95c6092084abbd4
spdarshan46 <spdarshan252@gmail.com>
2026-02-17 22:13:25 +0530

    changes

--- server/controllers/ocrController.js
+++ server/controllers/ocrController.js
@@ -13,21 +13,13 @@ exports.extractText = async (req, res) => {
         cleanedText = cleanedText.replace(/%/g, "");
         cleanedText = cleanedText.replace(/\s+/g, " ");
 
-        // Calories (both formats: "calories 200" OR "200 calories")
         const caloriesMatch =
             cleanedText.match(/calories\s*(\d+)/) ||
             cleanedText.match(/(\d+)\s*calories/);
 
-        // Sugar (handles: sugars 10g, total sugars less than 1g, sugar10g)
         const sugarMatch = cleanedText.match(/sugars?.*?(\d+)\s*g?/);
-
-        // Fat (handles total fat 10g, fat10g)
         const fatMatch = cleanedText.match(/total\s*fat.*?(\d+)\s*g?/);
-
-        // Sodium (handles sodium 120mg, sodium120mg)
         const sodiumMatch = cleanedText.match(/sodium.*?(\d+)\s*mg?/);
-
-        // Protein (handles protein 5g, protein5g)
         const proteinMatch = cleanedText.match(/protein.*?(\d+)\s*g?/);
 
         const nutritionData = {
